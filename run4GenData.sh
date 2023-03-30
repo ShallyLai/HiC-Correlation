1cad2a0d26d58a025151c7fcb23f556dbf54c6b9
ShallyLai <79987875+ShallyLai@users.noreply.github.com>
2023-03-30 13:43:07 +0800

    fourth_update

--- run4GenData.sh
+++ run4GenData.sh
@@ -1,27 +1,29 @@
 # tool to extract matrix from .hic files
 # file path of straw form Juicer
-# You need run a script by putting the absolute path 
+# must need the absolute path 
 straw="/Users/shallylai/Juicer/straw-master/bin/Mac/straw"
 
 # store the output file
 cd data 
+
 # $1: name of dataset
 mkdir $1
 cd ..
+
 # mapping tools' name
 declare -a Tools=("hicpro" "hicup" "hicpipe" "hickit" "5" "5SP")
 
 # path of Hi-C data
 HiC_data_Path="/Users/shallylai/Special_Topics/HiC"
 
-# loop HiC map normalization methods
-for nor in NONE VC VC_SQRT KR
+# loop for HiC map normalization methods
+for nor in NONE #VC VC_SQRT KR
 do
 	# chromosome we want to get
 	for chr in 1 22 
 	do
 		# HiC binszies
-		for bin_size in 10 25 
+		for bin_size in 10 #25 
 		do
 			echo "Process: " $nor $chr ${bin_size}000
 			# extract sparse upper triangular matrix format for mapping tools
